df380fe40d53ffcb56c2bed1b5b054325a6d43d9
Bella5968 <abitimenge@gmail.com>
2025-02-26 09:53:19 -0800

    s

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,11 +4,11 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "custom-user-agent/1.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        for post in response.json().get("data", {}).get("children", []):
-            print(post["data"]["title"])
-    else:
-        print(None)
+url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+headers = {"User-Agent": "custom-user-agent/1.0"}
+response = requests.get(url, headers=headers, allow_redirects=False)
+if response.status_code == 200:
+    for post in response.json().get("data", {}).get("children", []):
+    print(post["data"]["title"])
+else:
+    print(None)
