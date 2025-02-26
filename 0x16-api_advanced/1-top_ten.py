276e6979ee7fb0c23eaa6866d79997e67f194331
Bella5968 <abitimenge@gmail.com>
2025-02-26 10:00:25 -0800

    gg

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Query Reddit API to print top 10 hot post titles."""
+"""module to query the reddit api and print 10 hot post titles."""
+
 import requests
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts."""
-url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-headers = {"User-Agent": "custom-user-agent/1.0"}
-response = requests.get(url, headers=headers, allow_redirects=False)
-if response.status_code == 200:
-    for post in response.json().get("data", {}).get("children", []):
-    print(post["data"]["title"])
-else:
-    print(None)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "custom-user-agent/1.0"}
+
+    response = requests.get(url, headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get("data", {}).get("children", [])
+
+        for post in data:
+            print(post["data"]["title"])
+     else:
+         print(None)
